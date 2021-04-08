ce712fe0a384b0abc281b83675609118c2146df3
Domenico Armillotta <61909857+DomenicoArmillotta@users.noreply.github.com>
2021-04-08 15:12:14 +0200

    Update and rename sssssss.sql to model.sql

--- model.sql
+++ model.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.23, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.23, for macos10.15 (x86_64)
 --
--- Host: 127.0.0.1    Database: prova_db
+-- Host: 127.0.0.1    Database: asta
 -- ------------------------------------------------------
--- Server version	8.0.23-0ubuntu0.20.04.1
+-- Server version	8.0.23
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -24,7 +24,7 @@ DROP TABLE IF EXISTS `asta_aperta`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `asta_aperta` (
   `asta_id` int NOT NULL AUTO_INCREMENT,
-  `id_utente` int NOT NULL DEFAULT '999',
+  `id_utente` varchar(45) NOT NULL DEFAULT '999',
   `nome` varchar(45) DEFAULT NULL,
   `descrizione` varchar(200) DEFAULT NULL,
   `immagine` varchar(45) DEFAULT NULL,
@@ -34,8 +34,8 @@ CREATE TABLE `asta_aperta` (
   `scadenza` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`asta_id`),
   KEY `id_utente_idx` (`id_utente`),
-  CONSTRAINT `id_utente2` FOREIGN KEY (`id_utente`) REFERENCES `utente` (`id_utente`) ON UPDATE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `utente` FOREIGN KEY (`id_utente`) REFERENCES `utente` (`id_utente`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -44,7 +44,7 @@ CREATE TABLE `asta_aperta` (
 
 LOCK TABLES `asta_aperta` WRITE;
 /*!40000 ALTER TABLE `asta_aperta` DISABLE KEYS */;
-INSERT INTO `asta_aperta` VALUES (1,1,'domenico','bici','dsd',10,10,NULL,NULL),(2,2,'matteo','flessibile','asd',5,5,NULL,NULL),(3,3,'pietro','passamontagna','asd',5,5,NULL,NULL),(4,4,'michele','punteruolo','asd',12,12,NULL,NULL);
+INSERT INTO `asta_aperta` VALUES (1,'1','domenico','bici','dsd',10,10,NULL,NULL),(2,'2','matteo','flessibile','asd',5,5,NULL,NULL),(3,'3','pietro','passamontagna','asd',5,5,NULL,NULL),(4,'4','michele','punteruolo','asd',12,12,NULL,NULL);
 /*!40000 ALTER TABLE `asta_aperta` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -57,7 +57,7 @@ DROP TABLE IF EXISTS `asta_chiusa`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `asta_chiusa` (
   `asta_id` int NOT NULL AUTO_INCREMENT,
-  `id_utente` int NOT NULL,
+  `id_utente` varchar(45) NOT NULL,
   `nome` varchar(45) NOT NULL,
   `descrizione` varchar(45) NOT NULL,
   `immagine` varchar(45) NOT NULL,
@@ -66,8 +66,8 @@ CREATE TABLE `asta_chiusa` (
   `spedizione` varchar(45) DEFAULT NULL,
   `ora_apertura` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`asta_id`),
-  KEY `id_utente_idx` (`id_utente`),
-  CONSTRAINT `id_utente1` FOREIGN KEY (`id_utente`) REFERENCES `utente` (`id_utente`)
+  KEY `utente2_idx` (`id_utente`),
+  CONSTRAINT `utente2` FOREIGN KEY (`id_utente`) REFERENCES `utente` (`id_utente`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -88,11 +88,11 @@ DROP TABLE IF EXISTS `dettaglio_asta`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `dettaglio_asta` (
-  `offerente` int NOT NULL,
+  `offerente` varchar(45) NOT NULL,
   `data_offerta` varchar(45) NOT NULL,
   `importo` float DEFAULT NULL,
   `id_asta` int DEFAULT NULL,
-  PRIMARY KEY (`offerente`,`data_offerta`)
+  PRIMARY KEY (`data_offerta`,`offerente`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -102,7 +102,7 @@ CREATE TABLE `dettaglio_asta` (
 
 LOCK TABLES `dettaglio_asta` WRITE;
 /*!40000 ALTER TABLE `dettaglio_asta` DISABLE KEYS */;
-INSERT INTO `dettaglio_asta` VALUES (1123,'23/6/66',7.54,8),(1123,'23/7/66',7.54,8),(1123,'24/7/66',7.54,8),(3333,'aaaaaa',5,9),(4444,'s',8,9);
+INSERT INTO `dettaglio_asta` VALUES ('1123','23/6/66',7.54,8),('1123','23/7/66',7.54,8),('1123','24/7/66',7.54,8),('3333','aaaaaa',5,9),('4444','s',8,9);
 /*!40000 ALTER TABLE `dettaglio_asta` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -114,8 +114,7 @@ DROP TABLE IF EXISTS `utente`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `utente` (
-  `id_utente` int NOT NULL,
-  `username` varchar(45) DEFAULT NULL,
+  `id_utente` varchar(45) NOT NULL,
   `password` varchar(45) DEFAULT NULL,
   `indirizzo_spedizione` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id_utente`)
@@ -128,7 +127,7 @@ CREATE TABLE `utente` (
 
 LOCK TABLES `utente` WRITE;
 /*!40000 ALTER TABLE `utente` DISABLE KEYS */;
-INSERT INTO `utente` VALUES (1,'domenico','1234','via tali '),(2,'matteo','1234','via tali '),(3,'pietro','1234','via tali '),(4,'michele','1234','via tali ');
+INSERT INTO `utente` VALUES ('1','1234','via tali '),('2','1234','via tali '),('3','1234','via tali '),('4','1234','via tali ');
 /*!40000 ALTER TABLE `utente` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -141,4 +140,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-04-07 15:28:38
+-- Dump completed on 2021-04-08 14:56:58
